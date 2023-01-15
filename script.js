281cfcab11056b574c2c10ccf01165b07f51748e
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-01-15 17:01:13 -0300

    Ajuste nomes de variáveis e comentários

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Variáveis para manipulação do DOM.
 let elementosForm = document.querySelectorAll("input");
-let verificador = document.querySelector("#verificarGeral");
+let verifier = document.querySelector("#verificarGeral");
 var form = document.querySelector("#myForm");
 
 form.addEventListener("submit", (event) => {
@@ -10,14 +10,13 @@ form.addEventListener("submit", (event) => {
 
 const validateForm = () => {
   let valid = true;
-   //Transforma NodeList dos inputs em array e aplica metodo some para percorrê-lo.
+   //Transforma NodeList dos inputs em array e aplica método some para percorrê-lo.
   Array.from(elementosForm).some((element) => {
     if(element.value.length === 0){
       checkFields(null, element); //Chama função checkFields com o elemento alvo do input.
-      verificador.style.visibility = "visible";
-      verificador.innerHTML = "Campos obrigatórios não registrados.";
+      verifier.style.visibility = "visible";
+      verifier.innerHTML = "Campos obrigatórios não registrados.";
       valid = false;
-      return false; 
     }
   })
     if(valid) success();
@@ -32,26 +31,26 @@ let checkFields = (event = null, element = null) => {
 
 //Informa qual campo é necessário preencher.
 let fail = (element) => {
-  var elementoP = element.parentNode.children[2]; //busca através do pai (div) o elemento <p> para atribuição.
+  var elementoP = element.parentNode.children[2]; //Busca através da tag pai (div) a tag de parágrafo para atribuição da string de falha..
   elementoP.innerHTML = "*Campo Obrigatório*";
   elementoP.style.visibility = "visible";
 };
 
 //Informa o sucesso do cadastro.
 let success = () => {
-  verificador.style.visibility = "visible";
-  verificador.innerHTML = `<p class="success">Sucesso!</p>`;
+  verifier.style.visibility = "visible";
+  verifier.innerHTML = `<p class="success">Sucesso!</p>`;
   form.reset();
   reloadPage();
 };
 
 //Limpa o parágrafo alvo após detectar preenchimento via evento keyup ou quando o formulário é submetido.
 let cleanParagraph = (element) => {
-  var elementoP = element.parentNode.children[2]; //Seleciona o parágrafo referente à variável element passada como parâmetro.
+  var elementP = element.parentNode.children[2]; //Seleciona o parágrafo referente à variável element passada como parâmetro.
   element.innerHTML = "";
-  elementoP.style.visibility = "hidden";
-  verificador.style.visibility = "hidden";
-  verificador.innerHTML = "";
+  elementP.style.visibility = "hidden";
+  verifier.style.visibility = "hidden";
+  verifier.innerHTML = "";
 };
 
 //Recarrega a página em 3s.
